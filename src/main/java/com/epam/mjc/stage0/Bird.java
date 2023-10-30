bccca9b5e49d2220c308f11515e0bbdf458e26c4
Nodir Asomov <nasomov93@gmail.com>
2023-10-30 11:15:14 +0500

    hi

--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,20 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal{
+
+    public Bird() {
+        super("blue", 2, false);
+    }
+
+    @Override
+    public String getDescription() {
+        Animal animal = new Animal("blue", 2, false);
+        return animal.getDescription() + " Moreover, it has " + "2 wings and can fly.";
+    }
+
+    public static void main(String[] args) {
+        Animal bird = new Bird();
+        System.out.println(bird.getDescription());
+    }
 
 }
